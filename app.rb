ca402eac0f435cec436fe7db9088e09f0c7da44a
JP Hastings-Spital <jphastings@gmail.com>
2016-05-29 22:11:32 +0100

    All end points

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'sinatra/base'
 
 class ShutUpClaire < Sinatra::Base
-  get '/' do
-    send_file 'public/index.html'
-  end
-
   get '/css/colours.css' do
     content_type :css
     erb :colours, locals: {
       start: rand(360),
     }
+  end
+
+  get '*', provides: 'text/html' do
+    send_file 'public/index.html'
   end
 end
