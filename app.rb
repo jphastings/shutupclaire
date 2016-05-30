ea25e1b5ec41993f439c390ca7da57f47110be9e
JP Hastings-Spital <jphastings@gmail.com>
2016-05-30 11:23:03 +0100

    Various Claires

--- app.rb
+++ app.rb
@@ -1,14 +1,25 @@
 require 'sinatra/base'
+require 'tilt/erb'
+require 'yaml'
 
 class ShutUpClaire < Sinatra::Base
+  helpers do
+    def site(host)
+      site = host.split('.claire.').first
+      @@sites ||= YAML.load(open('etc/claires.yaml'))
+      @@sites[site] || @@sites['shutup']
+    end
+  end
+
   get '/css/colours.css' do
     content_type :css
-    erb :colours, locals: {
+    erb :colours, layout: nil, locals: {
       start: rand(360),
     }
   end
 
   get '*', provides: 'text/html' do
-    send_file 'public/index.html'
+    details = site(env['HTTP_HOST'])
+    erb details['type'].to_sym, locals: details
   end
 end
